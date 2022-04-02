1fb5c6f1776f107c2eeea63ee87482ca8ec87afe
hendrianiyan <hendrian582@gmail.com>
2022-04-02 12:11:47 +0700

    Menambahkan class GloassaryItem1302200104

---
+++ GlossaryItem1302200104.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace modul6_kelompok_5;
+public class GlossaryItem1302200104
+{
+    public void ReadJSON()
+    {
+        //path
+        string loc = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        //read Json file
+        string json = File.ReadAllText(loc + "\\jurnal6_3_1302200104.json");
+        //convert json
+        dynamic data = JsonConvert.DeserializeObject(json);
+        dynamic gentry = data.glossary.GlossDiv.GlossList.GlossEntry;
+        //print data
+        Console.WriteLine("ID : " + gentry.ID + "\nSortAs : " + gentry.SortAs + "\nGlossTerm : " + gentry.GlossTerm + "\nAcronym : " + gentry.Acronym + "\nAbbrev : " + gentry.Abbrev);
+
+
+    }
+}
