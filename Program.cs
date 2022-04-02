5af4baddf69ea9561f6fe15c1870401547c2e17e
realizyy <realizy@student.telkomuniversity.ac.id>
2022-04-02 13:16:22 +0800

    Update main program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using modul6_kelompok_5;
+
 Console.WriteLine("Hello, World!");
+DataMahasiswa1302204035 dmhs = new DataMahasiswa1302204035();
+dmhs.ReadJSON();
+Console.WriteLine("=====================");
+TeamMembers1302204035 teamme = new TeamMembers1302204035();
+teamme.ReadJSON();
+Console.WriteLine("=====================");
+GlossaryItem1302204035 gits = new GlossaryItem1302204035();
+gits.ReadJSON();
+Console.WriteLine("=====================");
