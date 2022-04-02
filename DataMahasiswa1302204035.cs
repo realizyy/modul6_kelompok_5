1cb9342e300bb334c5dc2c4d1ab707abb4f36e45
realizyy <realizy@student.telkomuniversity.ac.id>
2022-04-02 11:21:17 +0800

    menambahkan class DataMahasiswa1302204035

---
+++ DataMahasiswa1302204035.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace modul6_kelompok_5;
+
+public class DataMahasiswa1302204035
+{
+    public void ReadJSON()
+    {
+        //read json file
+        string jsonFile = File.ReadAllText(@"D:\Telyuuu\.Semester 4\Github\KPL\modul6_kelompok_5\jurnal6_1_nim.json");
+        //convert json to array
+        dynamic data = JsonConvert.DeserializeObject(jsonFile);
+        //print data
+        Console.WriteLine("Nama " + data.firstName + " " + data.lastName + " gender " + data.gender + " age " + data.age + " address : \n" + data.address.streetAddress +" "+ data.address.city +" "+ data.address.state);
+    }
+}
