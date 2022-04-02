a180b5bf1e46d6f93a69988ac067106b18acbd06
putrilarasxx@365.telkomuniversity.ac.id <putrilarasati440@gmail.com>
2022-04-02 10:57:25 +0700

    menambahkan class DataMahasiswa1302204011

---
+++ DataMahasiswa1302204011.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_kelompok_5
+{
+    public class DataMahasiswa1302204011
+    {
+        public void ReadJSON()
+        {
+            //read json file
+            string jsonFile = File.ReadAllText(@"C:\Users\asus\Downloads\Projects\modul6\modul6_kelompok_5\jurnal6_1_1302204011.json");
+            //convert json to array
+            dynamic data = JsonConvert.DeserializeObject(jsonFile);
+            //print data
+            Console.WriteLine("Nama " + data.firstName + " " + data.lastName + " gender " + data.gender + " age " + data.age + " address : \n" + data.address.streetAddress + " " + data.address.city + " " + data.address.state);
+        }
+    }
+}
