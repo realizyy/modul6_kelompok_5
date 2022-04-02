3cf996c3ed7ce1feba74f3bfa0519b13dd06eea9
realizyy <realizy@student.telkomuniversity.ac.id>
2022-04-02 12:30:29 +0800

    Menambahkan class TeamMembers1302204035

---
+++ TeamMembers1302204035.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace modul6_kelompok_5;
+
+public class TeamMembers1302204035
+{
+    
+    public void ReadJSON()
+    {
+        //path
+        string loc = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        //read json file
+        string jsonFile = File.ReadAllText(loc + "\\jurnal6_2_nim.json");
+        //convert json to array
+        dynamic data = JsonConvert.DeserializeObject(jsonFile);
+        //print data
+        Console.WriteLine("Team member list :");
+        foreach (var item in data.members)
+        {
+            Console.WriteLine(item.nim + " " + item.firstName + " " + item.lastName + " (" + item.age + " " + item.gender + ")" );
+        }
+    }
+}
