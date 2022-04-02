dab2cdee7623141fac684c1fe742de72fc148dba
putrilarasxx@365.telkomuniversity.ac.id <putrilarasati440@gmail.com>
2022-04-02 12:16:25 +0700

    GlossaryItem1302204011

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+modul6_kelompok_5.DataMahasiswa1302204011 mhs1 = new modul6_kelompok_5.DataMahasiswa1302204011();
+modul6_kelompok_5.TeamMembers1302204011 mhs2 = new modul6_kelompok_5.TeamMembers1302204011();
+modul6_kelompok_5.GlossaryItem1302204011 mhs3 = new modul6_kelompok_5.GlossaryItem1302204011();
+
+mhs1.ReadJSON();
+mhs2.ReadJSON();
+mhs3.ReadJSON();
