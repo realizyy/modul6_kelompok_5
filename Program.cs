c168574f317260f2a3b0ea5bfdd6e7f2a59372c0
hendrianiyan <hendrian582@gmail.com>
2022-04-02 12:17:30 +0700

    update program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+modul6_kelompok_5.DataMahasiswa1302200104 mhs = new modul6_kelompok_5.DataMahasiswa1302200104();
+mhs.ReadJSON();
+modul6_kelompok_5.TeamMembers1302200104 mem = new modul6_kelompok_5.TeamMembers1302200104();
+mem.ReadJSON();
+modul6_kelompok_5.GlossaryItem1302200104 gos = new modul6_kelompok_5.GlossaryItem1302200104();
+gos.ReadJSON();
